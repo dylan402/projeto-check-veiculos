b5e7f5ae2f954a9005a74a86f3ed8f94e6bf4db8
Dylan Santiago <dylansantiagomelo@gmail.com>
2021-03-21 21:01:09 -0300

    Configurando DynamoDB

---
+++ backend-veiculo/src/main/java/br/checkveiculos/persistencia/DynamoDBConfig.java
@@ -0,0 +1,38 @@
+package br.checkveiculos.persistencia;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
+@Configuration
+public class DynamoDBConfig {
+
+	@Value("${amazon.aws.accesskey}")
+	private String amazonAWSAccessKey;
+
+	@Value("${amazon.aws.secretkey}")
+	private String amazonAWSSecretKey;
+
+	public AWSCredentialsProvider amazonAWSCredentialsProvider() {
+		return new AWSStaticCredentialsProvider(amazonAWSCredentials());
+	}
+
+	@Bean
+	public AWSCredentials amazonAWSCredentials() {
+		return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+	}
+
+	@Bean
+	public AmazonDynamoDB amazonDynamoDB() {
+		return AmazonDynamoDBClientBuilder.standard().withCredentials(amazonAWSCredentialsProvider())
+				.withRegion(Regions.US_EAST_2).build();
+	}
+}
