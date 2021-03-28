b7e008d9ddab6780a8b81d64df204b432db82b30
Dylan Santiago <dylansantiagomelo@gmail.com>
2021-03-28 10:53:14 -0300

    Alterando pk para string

--- backend-cliente/src/main/java/br/checkveiculos/persistencia/ClienteRepository.java
+++ backend-cliente/src/main/java/br/checkveiculos/persistencia/ClienteRepository.java
@@ -1,12 +1,10 @@
 package br.checkveiculos.persistencia;
 
-import java.util.UUID;
-
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import br.checkveiculos.entidades.Cliente;
 
 @EnableScan()
-public interface ClienteRepository extends CrudRepository<Cliente, UUID> {
+public interface ClienteRepository extends CrudRepository<Cliente, String> {
 }
