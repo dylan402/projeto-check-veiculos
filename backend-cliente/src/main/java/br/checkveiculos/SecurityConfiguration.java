1e1e4be43dd78e5a33b1bd652a44b69fa60805d8
Vinícius Dabian <vinicius_dabian@hotmail.com>
2021-04-22 20:12:16 -0300

    Desabilitar csrf

--- backend-cliente/src/main/java/br/checkveiculos/SecurityConfiguration.java
+++ backend-cliente/src/main/java/br/checkveiculos/SecurityConfiguration.java
@@ -12,5 +12,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.httpBasic().disable();
+		httpSecurity.csrf().disable();
 	}
 }
